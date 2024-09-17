f67df4e69c20b9ea7911cb561b4efca914269631
CrisWorld <52689017+CrisWorld@users.noreply.github.com>
2024-09-17 22:02:00 +0700

    fix image

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -22,9 +22,7 @@ function AuthRootLayout({ children }: { children: React.ReactNode }) {
                     <Image
                         src={AuthBanner}
                         alt="Woman using smartphone"
-                        width={1440}
-                        height={1440}
-                        className='object-cover'
+                        className='h-screen'
                     />
                 </div>
             </div>
