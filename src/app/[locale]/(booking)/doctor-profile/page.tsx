65044499da9c9c8170beeeed0a64271803c1b2ef
CrisWorld <52689017+CrisWorld@users.noreply.github.com>
2024-09-17 23:38:52 +0700

    add User

---
+++ src/app/[locale]/(booking)/doctor-profile/page.tsx
@@ -0,0 +1,5 @@
+import DoctorProfile from "@/components/Core/modules/Calendar/doctor-profile";
+
+export default function Component() {
+    return (<DoctorProfile />);
+}
--- src/app/[locale]/(booking)/profile/page.tsx
+++
@@ -1,5 +0,0 @@
-import DoctorProfile from "@/components/Core/modules/Calendar/profile";
-
-export default function Component() {
-    return (<DoctorProfile />);
-}
