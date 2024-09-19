86ba129f545ca870926fcd5bb3c77f9e780f7c32
CrisWorld <52689017+CrisWorld@users.noreply.github.com>
2024-09-19 13:16:30 +0700

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/app/i18n/index.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
+const nextConfig = {
     compiler: {
         styledComponents: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
     },
     reactStrictMode: false,
     images: {
