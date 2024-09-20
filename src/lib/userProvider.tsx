d521b1ba0b27a30faa706c417645e5ca65e72d6c
Kaede Developer <vuvo070403@gmail.com>
2024-09-20 12:37:28 +0700

    Feat-Refresh Access Token

--- src/lib/userProvider.tsx
+++ src/lib/userProvider.tsx
@@ -8,10 +8,14 @@ import RingLoaderComponent from '@/components/Core/common/RingLoader'
 import { jwtDecode } from 'jwt-decode'
 import { JwtPayloadUpdated } from '@/components/Core/modules/Auth/SignIn'
 import { useRequestRefreshAccessTokenMutation } from '@/stores/services/auth'
+import { setLoaded, setLoading } from '@/stores/features/loading'
+import { message } from 'antd'
+import { useRouter } from 'next/navigation'
 
 function UserProvider({ children }: { children: React.ReactNode }) {
     const dispatch = useAppDispatch()
     const loading = useAppSelector((state) => state.loading.isLoading)
+
 
     useEffect(() => {
         const avatar = webStorageClient.get(constants.USER_AVATAR)
@@ -29,8 +33,10 @@ function UserProvider({ children }: { children: React.ReactNode }) {
     const _accessToken: string = webStorageClient.getToken() as string
     const _refreshToken = webStorageClient.getRefreshToken() as string
     const [requestRefreshAccessToken] = useRequestRefreshAccessTokenMutation()
-    console.log(_accessToken);
-    console.log(_refreshToken);
+    const router = useRouter();
+    const [messageApi, contextHolder] = message.useMessage();
+
+
     const handleRefreshAccessToken = async () => {
         if (_accessToken) {
             const claims = jwtDecode<JwtPayloadUpdated>(
@@ -38,15 +44,31 @@ function UserProvider({ children }: { children: React.ReactNode }) {
             )
             const expiredAt = claims.exp;
 
-            const bufferTime = 4 * 60
+            const bufferTime = 3 * 60
+            
             if (expiredAt! - Date.now() / 1000 < bufferTime) {
                 console.log(
-                    'Access token will expire within 4 minutes, refreshing token...',
+                    'Access token will expire within 3 minutes, refreshing token...',
                 )
+                messageApi.loading({
+                content: "Đang xác thực tài khoản...",
+                key: 'authVerification',
+            });
                 const result = await requestRefreshAccessToken({
                     refreshToken: _refreshToken!,
                 })
                 console.log("REFRESHED: ", result);
+                messageApi.success({
+                    content: "Tài khoản đã được xác thực thành công!",
+                    key: 'authVerification',
+                    duration: 2,
+                });
+            } 
+            if (expiredAt! < Date.now() / 1000) {
+                console.log("Access token expired, signing out...");
+                webStorageClient.removeAll()
+                message.destroy("Xác thực tài khoản thất bại! Vui lòng đăng nhập lại!")
+                router.push('/sign-in')
             }
         }
     }
