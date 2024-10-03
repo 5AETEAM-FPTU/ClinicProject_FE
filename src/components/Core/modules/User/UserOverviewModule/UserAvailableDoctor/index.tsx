0a4f88ab8d9f89259ba105bbaa9394a20b350bb5
From_VuVuDeveloper <vuvo070403@gmail.com>
2024-10-03 19:49:12 +0700

    Fixed Ui v1

--- src/components/Core/modules/User/UserOverviewModule/UserAvailableDoctor/index.tsx
+++ src/components/Core/modules/User/UserOverviewModule/UserAvailableDoctor/index.tsx
@@ -1,56 +1,35 @@
 'use client'
 
-import { Button } from 'antd'
 import { ChevronLeft, ChevronRight, ChevronsRight } from 'lucide-react'
 import Image from 'next/image'
-import ProfileBackground from '@public/landing/images/profile-background.png'
-import { Settings } from 'react-slick'
-import Slider from 'react-slick'
-import Doctor from '@public/landing/images/doctor.png'
-import StarIcon from '@public/landing/icons/StartIcon'
-import Hospital from '@public/landing/icons/hospital.svg'
-import Dollar from '@public/landing/icons/dollar.svg'
+import Slider, { Settings } from 'react-slick'
+
+import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.css'
 
 export default function UserAvailableDoctor() {
-    return (
-        <div className="h-[230px] rounded-xl bg-white text-[#003553] shadow-third">
-            <div className="flex items-center justify-between p-5">
-                <p className="text-[16px] font-bold">
-                    Bác sĩ đang trực tại phòng khám
-                </p>
-                <p className="flex items-center text-[10.4px] font-bold">
-                    Yêu cầu tư vấn{' '}
-                </p>
-            </div>
+    const doctors = [
+        {
+            name: 'Namae wa nan desuka?',
+            avatar: 'https://res.cloudinary.com/dy1uuo6ql/image/upload/v1726408359/vfsjhbdmrbfirjf2pfek.jpg',
+        },
+        {
+            name: 'Namae wa nan desuka hha?',
+            avatar: 'https://res.cloudinary.com/dy1uuo6ql/image/upload/v1726408359/vfsjhbdmrbfirjf2pfek.jpg',
+        }, 
+        {
+            name: 'Namae wa nan desuka aahha?',
+            avatar: 'https://res.cloudinary.com/dy1uuo6ql/image/upload/v1726408359/vfsjhbdmrbfirjf2pfek.jpg',
+        }, 
+    ]
 
-            <div>
-                {/* <AvailableDoctor /> */}
-            </div>
-        </div>
-    )
-}
-
-export function AvailableDoctor() {
     const settings = {
         dots: false,
-        infinite: true,
+        infinite: doctors.length > 1,
         speed: 500,
-        slidesToShow: 4,
+        slidesToShow: doctors.length === 1 ? 1 : 5,
         slidesToScroll: 1,
-        nextArrow: (
-            <NextArrow
-                onClick={function (): void {
-                    throw new Error('Function not implemented.')
-                }}
-            />
-        ),
-        prevArrow: (
-            <PrevArrow
-                onClick={function (): void {
-                    throw new Error('Function not implemented.')
-                }}
-            />
-        ),
+
         responsive: [
             {
                 breakpoint: 1024,
@@ -58,6 +37,7 @@ export function AvailableDoctor() {
                     slidesToShow: 3,
                 },
             },
+
             {
                 breakpoint: 768,
                 settings: {
@@ -72,114 +52,40 @@ export function AvailableDoctor() {
             },
         ],
     }
-    const doctors = [
-        {
-            position: 'Thạc sĩ - Bác sĩ',
-            name: 'Namae wa nan desuka?',
-            specialty: 'Bác sĩ chuyên khoa',
-            price: '150.000đ',
-            rating: 4.5,
-            patients: 100,
-            image: '###',
-        },
-        {
-            position: 'Thạc sĩ - Bác sĩ',
-            name: 'Namae wa nan desuka?',
-            specialty: 'Bác sĩ chuyên khoa',
-            price: '150.000đ',
-            rating: 4.5,
-            patients: 100,
-            image: '###',
-        },
-        {
-            position: 'Thạc sĩ - Bác sĩ',
-            name: 'Namae wa nan desuka?',
-            specialty: 'Bác sĩ chuyên khoa',
-            price: '150.000đ',
-            rating: 4.5,
-            patients: 100,
-            image: '###',
-        },
-        {
-            position: 'Thạc sĩ - Bác sĩ',
-            name: 'Namae wa nan desuka?',
-            specialty: 'Bác sĩ chuyên khoa',
-            price: '150.000đ',
-            rating: 4.5,
-            patients: 100,
-            image: '###',
-        },
-        {
-            position: 'Thạc sĩ - Bác sĩ',
-            name: 'Namae wa nan desuka?',
-            specialty: 'Bác sĩ chuyên khoa',
-            price: '150.000đ',
-            rating: 4.5,
-            patients: 100,
-            image: '###',
-        },
 
-        // Add more doctor objects here...
-    ]
     return (
-        <div className="h-[80px] w-[80px] items-center">
-            <Carousel settings={settings}>
-                {doctors.map((doctor, index) => (
-                    <div
-                        key={index}
-                        className="!flex h-[430px] w-[260px] !flex-col !items-center !justify-center"
-                    >
-                        <div className="h-[20%] w-[20%] overflow-hidden rounded-xl shadow-secondary">
-                            <div className="h-[253px] w-full">
-                                <div className="h-full w-full">
+        <div className="h-[230px] rounded-xl bg-white text-[#003553] shadow-third">
+            <div className="flex items-center justify-between p-5">
+                <p className="text-[16px] font-bold">
+                    Bác sĩ đang trực tại phòng khám
+                </p>
+                <p className="flex items-center gap-1 text-[12px] font-bold">
+                    Yêu cầu tư vấn <ChevronsRight size={16} />
+                </p>
+            </div>
+
+            <div className="slider-container flex w-full flex-row px-5">
+                <Carousel settings={settings}>
+                    {doctors.map((doctor, index) => {
+                        return (
+                            <div className="h-[200px] max-h-[140px] w-[93%] border-2 rounded-[12px] bg-white p-[10px]">
+                                <div className="h-[70%] w-full  rounded-[8px] overflow-hidden">
                                     <Image
-                                        src={Doctor}
-                                        alt="doctor"
+                                        src={doctor.avatar}
+                                        alt="avatar"
+                                        width={140}
+                                        height={140}
                                         className="h-full w-full object-cover"
-                                    ></Image>
+                                    />
                                 </div>
-                            </div>
-                            <div>
-                                <div className="flex w-full flex-row justify-between bg-secondary p-[10px]">
-                                    <p className="flex items-center gap-1 text-[12px] font-bold">
-                                        Đánh giá:{' '}
-                                        <span className="flex items-center justify-center gap-1 text-orange-600">
-                                            4.5{' '}
-                                            <span className="mb-[3px]">
-                                                <StarIcon />
-                                            </span>
-                                        </span>
-                                    </p>
-                                    <p className="flex items-center gap-1 text-[12px] font-bold">
-                                        Lượt khám:
-                                        <span className="flex items-center justify-center gap-1 text-orange-600">
-                                            100
-                                        </span>
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="flex flex-col gap-[4px] p-[10px]">
-                                <p className="text-[12px]">{doctor.position}</p>
-                                <p className="text-[14px] font-bold">
+                                <p className='font-bold line-clamp-2'>
                                     {doctor.name}
                                 </p>
-                                <div className="flex flex-row items-center gap-2">
-                                    <Image src={Hospital} alt="icon"></Image>
-                                    <p className="text-[12px] font-semibold text-secondaryDarker">
-                                        {doctor.specialty}
-                                    </p>
-                                </div>
-                                <div className="flex flex-row items-center gap-2">
-                                    <Image src={Dollar} alt="icon"></Image>
-                                    <p className="text-[12px] font-semibold text-orange-600">
-                                        150.000đ
-                                    </p>
-                                </div>
                             </div>
-                        </div>
-                    </div>
-                ))}
-            </Carousel>
+                        )
+                    })}
+                </Carousel>
+            </div>
         </div>
     )
 }
@@ -197,7 +103,7 @@ function Carousel({ settings, children }: CarrouselProps) {
                 (responsive) =>
                     responsive as import('react-slick').ResponsiveObject,
             )}
-            className="!flex items-center !gap-[30px]"
+            className="!flex w-full flex-row items-center !gap-[10px]"
         >
             {children}
         </Slider>
@@ -212,10 +118,10 @@ const NextArrow = (props: ArrowProps) => {
     const { onClick } = props
     return (
         <div
-            className="flex cursor-pointer items-center justify-center rounded-full border border-secondaryDarker p-1 transition-all"
+            className="flex cursor-pointer items-center justify-center p-1 transition-all"
             onClick={onClick}
         >
-            <ChevronRight className="text-secondaryDarker" />
+            <ChevronRight size={18} className="text-secondarySupperDarker" />
         </div>
     )
 }
@@ -224,10 +130,10 @@ const PrevArrow = (props: ArrowProps) => {
     const { onClick } = props
     return (
         <div
-            className="flex cursor-pointer items-center justify-center rounded-full border border-secondaryDarker p-1 transition-all"
+            className="flex cursor-pointer items-center justify-center p-1 transition-all"
             onClick={onClick}
         >
-            <ChevronLeft className="text-secondaryDarker" />
+            <ChevronLeft size={18} className="text-secondarySupperDarker" />
         </div>
     )
 }
--- src/components/Core/modules/User/UserOverviewModule/UserProfile/index.tsx
+++ src/components/Core/modules/User/UserOverviewModule/UserProfile/index.tsx
@@ -1,20 +1,23 @@
 'use client'
+import { useAppSelector } from '@/hooks/redux-toolkit'
 import { useGetUserProfileQuery } from '@/stores/services/user/userSettings'
 import BackGround from '@public/landing/images/profile-background.png'
 import dayjs from 'dayjs'
 import { CalendarRange, Settings, Smartphone, Split } from 'lucide-react'
+import Image from 'next/image'
+import { useEffect } from 'react'
 
 export type ShortUserProfileType = {
     avatarUrl: string
     fullName: string | null
     description: string | null
     gender: {
-        genderName:string;
+        genderName: string
     }
     dob: string | null
     phoneNumber: string
 }
- 
+
 export default function UserProfile() {
     const { result, isFetching, refetch } = useGetUserProfileQuery(undefined, {
         selectFromResult: ({ data, isFetching }) => {
@@ -25,10 +28,12 @@ export default function UserProfile() {
         },
     })
 
-    console.log(result);
+    useEffect(() => {
+        refetch();
+    }, [])
 
     return (
-        <div className="xl:h-[416px] h-fit rounded-xl">
+        <div className="h-fit rounded-xl xl:h-[416px]">
             <div className="shadow h-full rounded-lg bg-white p-4 shadow-third">
                 <div
                     style={{
@@ -36,39 +41,48 @@ export default function UserProfile() {
                     }}
                     className={`flex h-[160px] items-start justify-between rounded-[12px] bg-cover bg-center p-5`}
                 >
-                    <img
-                        src="https://i.pinimg.com/564x/92/26/5c/92265c40c8e428122e0b32adc1994594.jpg"
-                        alt="User"
-                        className="mb-4 h-[120px] w-[120px] rounded-[10px]"
-                    />
+                    <div className='w-[120px] h-[120px] rounded-[10px] overflow-hidden'>
+                        <Image
+                            src={result.avatarUrl!}
+                            alt="avatar"
+                            width={1200}
+                            height={1200}
+                            className="object-cover h-full w-full"
+                        ></Image>
+                    </div>
                 </div>
                 <div className="ml-[6px]">
                     <h2 className="mt-2 text-[24px] font-bold text-secondarySupperDarker">
-                        {result.fullName ?? "Chưa có"}
+                        {result.fullName ?? 'Chưa có'}
                     </h2>
                     <div className="mt-2 flex items-center">
                         <Split size={16} />
                         <p className="ml-[10px] text-[16px] font-medium text-secondarySupperDarker">
-                            Giới tính: {result.gender?.genderName ?? "Chưa có"}
+                            Giới tính: {result.gender?.genderName ?? 'Chưa có'}
                         </p>
                     </div>
                     <div className="mt-2 flex items-center">
                         <CalendarRange size={16} />
                         <p className="ml-[10px] text-[16px] font-medium text-secondarySupperDarker">
-                            Ngày sinh: {dayjs(result.dob).format("DD/MM/YYYY") ?? "Chưa có"}
+                            Ngày sinh:{' '}
+                            {dayjs(result.dob).format('DD/MM/YYYY') ??
+                                'Chưa có'}
                         </p>
                     </div>
 
                     <div className="mt-2 flex items-center">
                         <Smartphone size={16} />
                         <p className="ml-[10px] text-[16px] font-medium text-secondarySupperDarker">
-                            Số điện thoại: {result.phoneNumber ?? "Chưa có"}
+                            Số điện thoại: {result.phoneNumber ?? 'Chưa có'}
                         </p>
                     </div>
                     <div className="opacity-4 mx-[10px] mt-[18px] h-[0.8px] bg-[#003553]"></div>
-                    <p className="ml-[10px] text-[16px] font-semibold text-[#003553]">
-                        {result.description ?? "Chưa có"}
-                    </p>
+                    <p
+                        className="ml-[10px] text-[16px] font-semibold text-[#003553]"
+                        dangerouslySetInnerHTML={{
+                            __html: result?.description ?? 'Chưa có',
+                        }}
+                    ></p>
                 </div>
             </div>
         </div>
