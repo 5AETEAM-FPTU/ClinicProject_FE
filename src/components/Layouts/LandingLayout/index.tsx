3c8a843f18d578ce9eb6d568a8d58f5bd9bd2afe
From_VuVuDeveloper <vuvo070403@gmail.com>
2024-10-20 09:02:13 +0700

    Update Fixed Header

--- src/components/Layouts/LandingLayout/index.tsx
+++ src/components/Layouts/LandingLayout/index.tsx
@@ -1,18 +1,14 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
-import { Grid } from 'antd'
 import Footer from './Footer'
 import Reloading from './Reloading'
 import Header from './Header'
 import HeaderMobile from './HeaderMobile'
 
-const DynamicHeader = dynamic(() => import('./Header'), { ssr: false })
-const DynamicHeaderMobile = dynamic(() => import('./HeaderMobile'), { ssr: false })
-
 function LandingLayout({ children }: { children: React.ReactNode }) {
-    const screen = Grid.useBreakpoint();
     const [isLoading, setIsLoading] = useState(true);
+    const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
         const loadingTimeout = setTimeout(() => {
@@ -23,10 +19,20 @@ function LandingLayout({ children }: { children: React.ReactNode }) {
             clearTimeout(loadingTimeout);
         };
     }, []);
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.matchMedia('(max-width: 768px)').matches);
+        };
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
     return (
         <div>
             {isLoading ? <Reloading /> : null}
-            {screen.md ? <Header /> : <HeaderMobile />}
+            {isMobile ? <HeaderMobile /> : <Header />}
             {children}
             <Footer />
         </div>
