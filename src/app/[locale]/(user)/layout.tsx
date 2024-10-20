f61bb1dfe01a56942b907208da7850f06d2a380c
From_VuVuDeveloper <vuvo070403@gmail.com>
2024-10-20 11:36:01 +0700

    Update Metadata

--- src/app/[locale]/(user)/layout.tsx
+++ src/app/[locale]/(user)/layout.tsx
@@ -1,10 +1,17 @@
 import UserLayout from '@/components/Layouts/UserLayout'
 import constants from '@/settings/constants'
 import { getCookie } from 'cookies-next'
+import { Metadata } from 'next'
 import dynamic from 'next/dynamic'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import React from 'react'
+
+export const metadata: Metadata = {
+    title: "P-CLINIC",
+    description: "P-CLINIC",
+    icons: "/icons/layout/p-clinic.png",
+};
 
 function UserRootLayout({ children }: { children: React.ReactNode }) {
     const _accessToken_Cookie = getCookie(constants.ACCESS_TOKEN, { cookies })    
