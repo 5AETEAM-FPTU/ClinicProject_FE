04d57ee6cefe8ea6df63a3cce7648f23ef54d36d
From_VuVuDeveloper <vuvo070403@gmail.com>
2024-11-07 17:03:33 +0700

    Vu-Fix appointment on day

--- src/components/Core/modules/Doctor/DoctorVisitOnDayModule/index.tsx
+++ src/components/Core/modules/Doctor/DoctorVisitOnDayModule/index.tsx
@@ -14,7 +14,7 @@ export default function DoctorVisitInDayModule() {
 
     const { appointments, refetch, isFetching } = 
     useGetAppointmentOnDayQuery(
-        { date: "2024-10-23T15:44:16.142Z"},
+        { date: now},
         {
             selectFromResult: ({ data, isFetching }) => ({
                 appointments: data?.body?.appointment ?? [],
